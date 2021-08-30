963e5484227db97a4dcaa3694a343590718ee8c3
Keyman21 <88323879+Keyman21@users.noreply.github.com>
2021-08-30 22:38:59 +0300

    updated C programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,23 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 		int d;
 
-		for (d = 0; d < 90; d++)
-		{
-				for ()
-				{
-					putchar((d / 10) + '0');						       putchar((d % 10) + '0');																     }					
-				if (d != 89) 			               				       {
-															 putchar(',');								        putchar(' ');
-																						              }
+			for (d = 0; d < 90; d++)
+					{
+								for ()
+											{
+															putchar((d / 10) + '0');
+																		putchar((d % 10) + '0');
+																				}
+										if (d != 89)
+													{
+																	putchar(',');
+																				putchar(' ');
+																						}
 											}
-	 putchar('\n');
-	 return (0);
+				putchar('\n');
+					return (0);
 }

7cc9e3e9e7b5dddf5346183859c6a20cd6497615
Keyman21 <88323879+Keyman21@users.noreply.github.com>
2021-09-01 17:35:50 +0300

    edit and change the pattern

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * print_alphabet - print the string 'abcdefghijklmnopqrstuvwxyz'
- *
- * Return: 0 on success.
- */
+* print_alphabet_x10 - print the string 'abcdefghijklmnopqrstuvwxyz' 10 times
+*
+* Return: 0 on success.
+*/
 void print_alphabet_x10(void)
 {
-		char c;
-		int i;
+	char c;
+	int i;
 
-		for (i= 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
 		{
-			for (c = 'a'; c <= 'z'; c++)
-			{
-				_putchar(c);
-			}
-			_putchar('\n');
+			_putchar(c);
 		}
+	_putchar('\n');
+	}
 }
+

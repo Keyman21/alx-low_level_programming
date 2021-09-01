8a146478944f021dd7e8917240c32eeb9f2342c8
Keyman21 <88323879+Keyman21@users.noreply.github.com>
2021-09-01 17:21:31 +0300

    add comment on file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,23 +4,23 @@
 *
 **/
 void jack_bauer(void)
-		{
-				int hours = 0;
-					int minutes = 0;
+	{
+	int hours = 0;
+	int minutes = 0;
 
-						while (hours < 24)
-									{
-												minutes = 0;
-														while (minutes < 60)
-																		{
-																						_putchar((hours / 10) + '0');
-																									_putchar((hours % 10) + '0');
-																												_putchar(':');
-																															_putchar((minutes / 10) + '0');
-																																		_putchar((minutes % 10) + '0');
-																																					_putchar('\n');
-																																								minutes++;
-																																											}
-																hours++;
-																		}
+	while (hours < 24)
+		{
+		minutes = 0;
+		while (minutes < 60)
+			{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+			minutes++;
+			}
+		hours++;
 		}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _isalpha(int c);
 int print_sign(int n);
 void print_to_98(int n);
 void times_table(void);
+void jack_bauer(void);

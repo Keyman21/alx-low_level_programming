3a76dc8d123985d5d11808cf7b69ccd61abd50a1
Keyman21 <88323879+Keyman21@users.noreply.github.com>
2021-09-01 17:30:13 +0300

    edit file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,22 +5,22 @@
 * Return: 1 if n is greater than 0, 0 if n is 0, -1 if n is less than 0
 **/
 int print_sign(int n)
-		{
-				if (n > 0)
-						{
-								_putchar(43);
-									return (1);
+	{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
 
-										}
-					else if (n == 0)
-							{
-									_putchar(48);
-										return (0);
-											}
-						else if (n <= 0)
-								{
-										_putchar(45);
-											return (-1);
-												}
-						return (0);
-		}
+	}
+	else if (n == 0)
+	{
+	_putchar(48);
+	return (0);
+	}
+	else if (n <= 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+return (0);
+}

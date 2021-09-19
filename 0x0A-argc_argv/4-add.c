0bd7f9ad274819d3ef71ddeb6e01f36fb8ade089
Keyman21 <88323879+Keyman21@users.noreply.github.com>
2021-09-19 12:18:19 +0300

    update add c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+ * main -   adds positive numbers.
+ * @argc: number of argument passed to the main function.
+ * @argv: an arrray that hold argument passed to the main function.
+ * Return: 0 mean success.
+ *
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
-	if (argc < 1)
+	if (argc < 2)
+	{
+		printf("0\n");
 		return (0);
-
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		sum += atoi(argv[i]);
+		sum = sum + atoi(argv[i]);
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }

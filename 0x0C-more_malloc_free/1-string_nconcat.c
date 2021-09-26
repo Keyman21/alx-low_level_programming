201e5188bc26303ddabefa70bfb03a7ee24e4ad3
Keyman21 <88323879+Keyman21@users.noreply.github.com>
2021-09-26 11:26:48 +0300

    add file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,12 +1,13 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
 
 /**
- * *string_nconcat -  concatenates two strings.
- * @s1: string pointer.
- * @s2: string pointer.
- * @n: size in byte .
+ * string_nconcat - concatenates two strings.
+ * @s1: first string to copy
+ * @s2: second string to copy
+ * @n: number of bytes of s2 to copy
  *
- * Return: return ptr.
+ * Return: char pointer to newly allocated place in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -17,37 +18,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i = 0;
 	else
 	{
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-		}
+		for (i = 0; s1[i]; i++)
+			;
 	}
 	if (s2 == NULL)
 		j = 0;
 	else
 	{
-		for (j = 0; s2[j] != '\0'; j++)
-		{
-		}
+		for (j = 0; s2[j]; j++)
+			;
 	}
-
 	if (j > n)
-	{
 		j = n;
-	}
-
 	s = malloc(sizeof(char) * (i + j + 1));
-
 	if (s == NULL)
 		return (NULL);
 	for (k = 0; k < i; k++)
-	{
 		s[k] = s1[k];
-	}
 	for (k = 0; k < j; k++)
-	{
 		s[k + i] = s2[k];
-	}
-
 	s[i + j] = '\0';
 	return (s);
 }
